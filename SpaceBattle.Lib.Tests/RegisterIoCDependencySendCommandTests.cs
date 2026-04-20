ee5e1776fde38ecd863212dd78f2454e68eaa871
Ksenia_Firstova <ksu0655@mail.ru>
2026-04-20 19:34:28 +0600

    feat: register Commands.Send dependency in IoC

---
+++ SpaceBattle.Lib.Tests/RegisterIoCDependencySendCommandTests.cs
@@ -0,0 +1,27 @@
+using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class RegisterIoCDependencySendCommandTests
+{
+    public RegisterIoCDependencySendCommandTests()
+    {
+        new InitCommand().Execute();
+    }
+
+    [Fact]
+    public void RegisterIoCDependencySendCommand_DependencyResolves()
+    {
+        var registerCommand = new RegisterIoCDependencySendCommand();
+        registerCommand.Execute();
+        
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+        
+        var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Send", mockCommand.Object, mockReceiver.Object);
+        
+        Assert.IsType<SendCommand>(resolvedCommand);
+    }
+}
---
+++ SpaceBattle.Lib/RegisterIoCDependencySendCommand.cs
@@ -0,0 +1,23 @@
+using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencySendCommand : ICommand  
+{
+    public void Execute()
+    {
+        var registerDependencyCommand = Ioc.Resolve<App.ICommand>(  
+            "IoC.Register",
+            "Commands.Send",
+            (object[] args) =>
+            {
+                var command = (ICommand)args[0];
+                var receiver = (ICommandReceiver)args[1];
+                return new SendCommand(command, receiver);
+            }
+        );
+        
+        registerDependencyCommand.Execute();  
+    }
+}
