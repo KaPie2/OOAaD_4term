73100ba89c1733d4905a3814f303866fdcc2ca30
Ksenia_Firstova <ksu0655@mail.ru>
2026-04-21 15:18:29 +0600

    feat: add CommandInjectable dependency registration

---
+++ SpaceBattle.Lib.Tests/RegisterDependencyCommandInjectableCommandTests.cs
@@ -0,0 +1,28 @@
+using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class RegisterDependencyCommandInjectableCommandTests
+{
+    public RegisterDependencyCommandInjectableCommandTests()
+    {
+        new InitCommand().Execute();
+    }
+
+    [Fact]
+    public void RegisterDependencyCommandInjectableCommand_DependencyResolves()
+    {
+        var registerCommand = new RegisterDependencyCommandInjectableCommand();
+        
+        registerCommand.Execute();
+        
+        var exception1 = Record.Exception(() => Ioc.Resolve<ICommand>("Commands.CommandInjectable"));
+        var exception2 = Record.Exception(() => Ioc.Resolve<ICommandInjectable>("Commands.CommandInjectable"));
+        var exception3 = Record.Exception(() => Ioc.Resolve<CommandInjectableCommand>("Commands.CommandInjectable"));
+        
+        Assert.Null(exception1);
+        Assert.Null(exception2);
+        Assert.Null(exception3);
+    }
+}
---
+++ SpaceBattle.Lib/RegisterDependencyCommandInjectableCommand.cs
@@ -0,0 +1,21 @@
+using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterDependencyCommandInjectableCommand : ICommand
+{
+    public void Execute()
+    {
+        var registerCommand = Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Commands.CommandInjectable",
+            (object[] args) =>
+            {
+                return new CommandInjectableCommand();
+            }
+        );
+        
+        registerCommand.Execute();
+    }
+}
