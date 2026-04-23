74cfe896c4f789f01bdbf97fb111382570d7b58e
Olesya <olegha2000@gmail.com>
2026-04-23 11:41:38 +0600

    format: fix format

--- SpaceBattle.Lib.Tests/AngleTests.cs
+++ SpaceBattle.Lib.Tests/AngleTests.cs
@@ -10,18 +10,18 @@ public class AngleTests
     [Fact]
     public void AddingAnglesReturnsCorrectAnswer()
     {
-        var angle1 = new Angle(new int[] {5, 8});
-        var angle2 = new Angle(new int[] {7, 8});
+        var angle1 = new Angle(new int[] { 5, 8 });
+        var angle2 = new Angle(new int[] { 7, 8 });
 
         Angle res = angle1 + angle2;
-        Assert.Equal(new int[]{4, 8}, res.Degrees);
+        Assert.Equal(new int[] { 4, 8 }, res.Degrees);
     }
 
     [Fact]
     public void EqualsReturnsTrueIfAnglesAreEqual()
     {
-        var angle1 = new Angle(new int[] {15, 8});
-        var angle2 = new Angle(new int[] {23, 8});
+        var angle1 = new Angle(new int[] { 15, 8 });
+        var angle2 = new Angle(new int[] { 23, 8 });
 
         Assert.True(angle1.Equals(angle2));
     }
@@ -29,8 +29,8 @@ public class AngleTests
     [Fact]
     public void EqualsReturnsFalseIfAnglesNotEqual()
     {
-        var angle1 = new Angle(new int[] {1, 8});
-        var angle2 = new Angle(new int[] {2, 8});
+        var angle1 = new Angle(new int[] { 1, 8 });
+        var angle2 = new Angle(new int[] { 2, 8 });
 
         Assert.False(angle1.Equals(angle2));
     }
@@ -38,8 +38,8 @@ public class AngleTests
     [Fact]
     public void EqualsOperatorReturnsTrueIfAnglesNotEqual()
     {
-        var angle1 = new Angle(new int[] {1, 8});
-        var angle2 = new Angle(new int[] {2, 8});
+        var angle1 = new Angle(new int[] { 1, 8 });
+        var angle2 = new Angle(new int[] { 2, 8 });
 
         Assert.True(angle1 != angle2);
     }
@@ -47,8 +47,8 @@ public class AngleTests
     [Fact]
     public void EqualsOperatorReturnsTrueIfAnglesEqual()
     {
-        var angle1 = new Angle(new int[] {15, 8});
-        var angle2 = new Angle(new int[] {23, 8});
+        var angle1 = new Angle(new int[] { 15, 8 });
+        var angle2 = new Angle(new int[] { 23, 8 });
 
         Assert.True(angle1 == angle2);
     }
